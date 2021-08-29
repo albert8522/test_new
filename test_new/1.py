21a5d8e4ef5c5a0738459840cc61567364105ebc
albert <albert8522@gmail.com>
2021-08-29 14:59:14 +1200

    push test commit

--- test_new/1.py
+++ test_new/1.py
@@ -1,5 +1,7 @@
-print('hello python and git ');
-print("hello python and git 2");
-print("hello python and git3");
-print("hello python and git4");
-print("master test");
+print('hello python and git ')
+print("hello python and git 2")
+print("hello python and git3")
+print("hello python and git4")
+print("hot-fix test")
+print("master test")
+print("push test")
